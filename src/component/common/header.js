1020cda70e2a889bbb8eaacdd5732cbbf6493cbc
cansolve <tongxiang608@gmail.com>
2018-11-26 19:59:36 +0800

    修改跳转

--- src/component/common/header.js
+++ src/component/common/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom'
 class Header extends React.Component {
   constructor(props) {
     super(props);
@@ -14,13 +14,22 @@ class Header extends React.Component {
   render() {
     
     return (
-      <header className="navbar">
+      
+      <div className="navbar">
         <div className="container">
           <div className="icon-wrap"></div>
-          <div className="menus"></div>
+          <div className="menus">
+            <ul className="navbar-nav">
+              <li className="menu-item"><Link to="/swiper">首页</Link></li>
+              <li className="menu-item">热门</li>
+              <li className="menu-item">生活分享</li>
+              <li className="menu-item">留言</li>
+              <li className="menu-item">开源技术</li>
+            </ul>
+          </div>
           <div className="search-wrap"></div>
         </div>
-      </header>
+      </div>
     )
   }
 }
